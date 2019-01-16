e0b888f40c581dec660c57686caa08c49c79d340
atyler88 <a.tyler88@gmail.com>
2019-01-16 12:29:27 -0800

    Added 3 dummy components

---
+++ blog/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
+        </div>
+    )
+}
+
+export default Contact
